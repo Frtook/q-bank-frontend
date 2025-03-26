cfaa74636812938de5f9cf79c733e8e5365fc741
frtook <frtook@hotmail.com>
2025-03-26 23:08:51 +0300

    test commit

--- app/(dashboard)/subject/page.tsx
+++ app/(dashboard)/subject/page.tsx
@@ -7,7 +7,7 @@ import AddSubjectDialog from "./AddSubjectDialog";
 export default function Page() {
   const { data } = useGetSubject();
   return (
-    <div className="p-4 block m-2 h-1 w-4">
+    <div className="m-2 block h-1 w-4 p-4">
       <div className="flex justify-between">
         <h1>All Subject</h1>
         <AddSubjectDialog />
